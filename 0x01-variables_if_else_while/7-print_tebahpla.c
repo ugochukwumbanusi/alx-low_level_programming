3f07d66e27be3b0af283b95bbf02bfde1ecb9f52
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-07-14 16:48:47 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,18 @@
-#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
- * Description: 'print alphabets in reverse'
- * Return: Always 0
+ * Description: 'print alphabets in reverse
+ * Return: Always
  */
 int main(void)
 {
 	int n = 122;
 
-	while (n >= 97)
+	while (n  >= 97)
 	{
 		putchar(n);
 		n--;
 	}
-	putchar('/n');
+	putchar('\n');
 	return (0);
 }
