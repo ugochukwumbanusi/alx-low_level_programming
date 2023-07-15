48bde3ebf38abd1e2e103081b998d1fdc68b0ba4
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-07-15 02:05:15 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-	int n;
-
-        for (n = 0; n <= 9; n++)
-        {
-		putchar(n + 48);
-	}
-        putchar('\n');
-        return (0);
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n + 48);
+}
+putchar('\n');
+return (0);
 }
