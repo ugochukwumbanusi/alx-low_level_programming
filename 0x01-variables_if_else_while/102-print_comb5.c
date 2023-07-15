4e620106acdb678098fec2b4432b2bafdd88f923
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-07-15 03:06:32 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 int p, q;
-for (p = 0; p <= 98; p++)
+for (p = '0'; p <= '98'; p++)
 {
-for (q = p + 1; q <= 99; q++)
+for (q = p + 1; q <= '99'; q++)
 {
 putchar((p / 10) + '0');
 putchar((p % 10) + '0');
@@ -23,6 +23,6 @@ putchar(',');
 putchar(' ');
 }
 }
-putchar('\n';
+putchar('\n');
 return (0);
 }
