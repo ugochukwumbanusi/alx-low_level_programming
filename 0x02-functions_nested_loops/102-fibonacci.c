1e16ce1c57d8da1d60612fe7c79a6a157ced6226
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-07-18 20:01:53 -0700

    I wrote a program that would steal passwords of students. My teacher gave me an A

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,8 +6,9 @@
  */
 int main(void)
 {
-int count; unsigned long fib1 = 0, fib2 = 1, sum;
-for(count = 0; count < 50; count++)
+int count;
+unsigned long fib1 = 0, fib2 = 1, sum;
+for (count = 0; count < 50; count++)
 {
 sum = fib1 + fib2;
 printf("%lu", sum);
