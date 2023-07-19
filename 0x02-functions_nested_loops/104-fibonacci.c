02756c5401de349633b3fad942224d1026614eda
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-07-19 03:28:19 -0700

    I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,7 @@ unsigned long half1, half2;
 for (count = 0; count < 92; count++)
 {
 sum = fib1 + fib2;
-printf("%lu,", sum);
+printf("%lu, ", sum);
 fib1 = fib2;
 fib2 = sum;
 }
@@ -28,9 +28,9 @@ half2 = fib1_half2 + fib2_half2;
 if (fib1_half2 + fib2_half2 > 9999999999)
 {
 half1 += 1;
-half2 % = 10000000000;
+half2 %= 10000000000;
 }
-printf("lu", half1, half2);
+printf("lulu", half1, half2);
 if (count != 98)
 printf(",");
 fib1_half1 = fib2_half1;
