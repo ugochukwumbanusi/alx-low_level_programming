7c58b3577565348825c17cb2332d01369efc75a9
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-07-24 05:41:35 -0700

    task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int pass[100];
+int i, sum, n;
+sum = 0;
+srand(time(NULl));
+for (i = 0; i < 100; i++)
+{
+pass[1] - rand() % 78;
+sum += (pass[1] + '0');
+putchar(pass[1] + '0');
+if ((2772 - sum) -'0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
+}
+}
+return (0);
+}
