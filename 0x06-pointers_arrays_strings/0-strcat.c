dcc3645b73e67cd843d1803f3408f68cb71bd4d0
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-07-28 16:36:01 +0400

    task 10

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- * Return: void
- */
-char *_strcat(char *dest, char *src)
+int main(void)
 {
-	int i;
-	int j;
+  int n;
+  int a[5];
+  int *p;
 
-	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
-
-	dest[i] = '\0';
-	return (dest);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
