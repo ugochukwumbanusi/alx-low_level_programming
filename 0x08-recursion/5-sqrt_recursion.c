7f2632f08eec69e9b52bc94da33924f42b95569d
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-08-02 12:11:18 -0700

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * sqrt_a - return the natural square root of
+ * a number
+ * @n: input number
+ * @c: iterator
+ * Return: square root or -1
+ */
+
+int sqrt_a(int a, int b)
+{
+	if (b * b == a)
+	{
+		return (b);
+	}
+	else if (b * b > a)
+	{
+		return (-1);
+
+	}
+	return (sqrt_a(a, b + 1));
+}
+/**
+ * sqrt_recursion - return the natural square root of
+ * a number
+ * @n: input number
+ * Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_a(n, 0));
+}
