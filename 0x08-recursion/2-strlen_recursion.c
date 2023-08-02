4c4cd90c015d10366f5d73c3dd2ec6330ce818da
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-08-02 05:15:36 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,12 +5,12 @@
  * Return: The length of the string
  */
 int _strlen_recursion(char *s)
-
-	if (*s)
+{
+	if (*s =='\n')
 	{
-		return (1 + _strlen_recursion(s + 1));
+		return (0);
 	}
-	else
-	}
-	return (0);
+
+	s++;
+	return (_strlen_recursion(s) + 1);
 }
