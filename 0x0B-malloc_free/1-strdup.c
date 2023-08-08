f55f637c181f14435017deb257884a44f193132d
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-08-08 00:27:20 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,21 +1,23 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
 /**
- * _strdup - dupicate to string
+ * _strdup - Dupicate a string
  * @str: the string to duplicate
+ * @s: char to assign
  * Return: the string duplicated
  */
 char *_strdup(char *str)
-
+{
+int a = 0, i = 1;
+char *s;
 if (str == NULL)
 return (NULL);
 while (str[i])
 {
 i++;
 }
-S = malloc((sizeof(char) * i) + 1);
+s = malloc((sizeof(char) * i) +1);
 if (s == NULL)
 return (NULL);
 while (a < i)
