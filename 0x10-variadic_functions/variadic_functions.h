0cd3e6affe13c9e1046854155b83d6ea4f263474
ugochukwumbanusi <ugochukwumbanusi@gmail.com>
2023-08-17 16:48:46 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,8 @@
-#ifndef "MAIN_H"
-#define "MAIN_H"
+#ifndef _VAR_FUNC_H
+#define _VAR_FUNC_H
+
+#include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
